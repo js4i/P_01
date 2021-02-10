8cd62e07dc5d437878d5e573da1618b15072dd33
js4i <j.silva@4intelligence.com.br>
2021-02-10 14:30:50 -0300

    style: 💄 atualizar class settings in config

--- app/config/settings.py
+++ app/config/settings.py
@@ -9,12 +9,8 @@ class Settings(BaseSettings):
     API_V1: str = "/api/v1"
 
 
-    LOCAL_TESTING: Optional[str] = False
     DATABASE_URL_LOCAL_TESTING: Optional[str] = os.environ.get('DATABASE_URL_LOCAL_TESTING')
     DATABASE_URL: Optional[str] = os.environ.get('DATABASE_URL')
-
-    class Config:
-        env_file = ".env"
 
 
 settings = Settings()
